efe996de12c1f67e46a04c93b38354e7b46d1712
Remi Beucherie <beucherie.r@gmail.com>
2019-11-07 15:13:45 +0100

    back ok

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
-// config/webpack/environment.js
-const { environment } = require('@rails/webpacker')
-
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+require 'json'
+puts "Cleaning DB..."
+Ingredient.destroy_all
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+puts "Seeding ingredients from API..."
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
+puts "Done !"
